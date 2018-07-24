72138e3370185019a37302f9828690c0d874ac84
randomsilo <randomsilo@gmail.com>
2018-07-24 16:09:01 -0500

    Adding SSL

--- README.md
+++ README.md
@@ -4,7 +4,15 @@ restful web api for controlling rpi gpio
 
 # run commands
 
+## start web server
+
 ```
 export FLASK_APP=rpi-api-gpio.py
 flask run --host=0.0.0.0
+```
+
+# make ssl key
+
+```
+sudo openssl req -new -newkey rsa:2048 -nodes -keyout ssl.key -out ssl.crt
 ```
--- rpi-api-gpio.py
+++ rpi-api-gpio.py
@@ -1,6 +1,14 @@
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('ssl.crt', 'ssl.key')
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == "__main__":
+    context = ('cert.crt', 'key.key')
+    app.run(host='0.0.0.0', port=443, ssl_context=context, threaded=True, debug=True)
