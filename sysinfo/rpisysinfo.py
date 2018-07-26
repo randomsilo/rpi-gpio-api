4404e18a47ab2c4a5a294ce99da4af7f491ef6ed
randomsilo <randomsilo@gmail.com>
2018-07-25 21:05:06 -0500

    add line continuation chars

--- sysinfo/rpisysinfo.py
+++ sysinfo/rpisysinfo.py
@@ -45,11 +45,11 @@ class RpiSysInfo():
     def get_boot_info(self):
         item = {'start_time': 'Na','running_since':'Na'}
         try:
-            item['running_duration'] = subprocess.check_output(['uptime -p'], shell=True)
-                .decode('utf8')
+            item['running_duration'] = subprocess.check_output(['uptime -p'], shell=True)\
+                .decode('utf8')\
                 .replace('\n', '')
             item['start_time'] = subprocess.check_output(['uptime -s'], shell=True)
-                .decode('utf8')
+                .decode('utf8')\
                 .replace('\n', '')
         except Exception as ex:
             print(ex)
@@ -59,17 +59,17 @@ class RpiSysInfo():
     def get_memory_usage_info(self):
         try:
             item = {'total': 0,'used': 0,'free': 0, 'available': 0 }
-            item['total']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $2'}"], shell=True)
-                .decode('utf8')
+            item['total']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $2'}"], shell=True)\
+                .decode('utf8')\
                 .replace('\n', '')
-            item['used']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $3'}"], shell=True)
-                .decode('utf8')
+            item['used']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $3'}"], shell=True)\
+                .decode('utf8')\
                 .replace('\n', '')
-            item['free']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $4'}"], shell=True)
-                .decode('utf8')
+            item['free']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $4'}"], shell=True)\
+                .decode('utf8')\
                 .replace('\n', '')
-            item['free']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $7'}"], shell=True)
-                .decode('utf8')
+            item['free']=  subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $7'}"], shell=True)\
+                .decode('utf8')\
                 .replace('\n', '')
         except Exception as ex:
             print(ex)
@@ -77,17 +77,17 @@ class RpiSysInfo():
             return dict(memory_usage_info = item)
 
     def get_os_name(self):
-        os_info = subprocess.check_output("cat /etc/*-release | grep PRETTY_NAME | cut -d= -f2", shell=True)
-            .decode('utf8')
-            .strip()
+        os_info = subprocess.check_output("cat /etc/*-release | grep PRETTY_NAME | cut -d= -f2", shell=True)\
+            .decode('utf8')\
+            .strip()\
             .replace('\"', '')
         return dict(os_name=os_info)
 
     def get_cpu_usage_info(self):
         item = {'in_use': 0}
         try:
-            item['in_use'] = subprocess.check_output("top -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4 }'", shell=True)
-                .decode('utf8')
+            item['in_use'] = subprocess.check_output("top -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4 }'", shell=True)\
+                .decode('utf8')\
                 .replace('\n', '')
         except Exception as ex:
             print(ex)
@@ -95,32 +95,32 @@ class RpiSysInfo():
             return dict(cpu_usage_info = item)
 
     def get_cpu_processor_count(self):
-        proc_info = subprocess.check_output("nproc", shell=True)
+        proc_info = subprocess.check_output("nproc", shell=True)\
             .decode('utf8')
             .strip()
             .replace('\"', '')
         return dict(cpu_processor_count=proc_info)
 
     def get_cpu_core_frequency(self):
-        core_frequency = subprocess.check_output("vcgencmd get_config arm_freq | cut -d= -f2", shell=True)
-            .decode('utf8')
-            .strip()
+        core_frequency = subprocess.check_output("vcgencmd get_config arm_freq | cut -d= -f2", shell=True)\
+            .decode('utf8')\
+            .strip()\
             .replace('\"', '')
         return dict(cpu_core_frequency=core_frequency)
 
     def get_cpu_core_volt(self):
-        core_volt = subprocess.check_output("vcgencmd measure_volts| cut -d= -f2", shell=True)
-            .decode('utf8')
-            .strip()
+        core_volt = subprocess.check_output("vcgencmd measure_volts| cut -d= -f2", shell=True)\
+            .decode('utf8')\
+            .strip()\
             .replace('\"', '')
         return dict(cpu_core_volt=core_volt)
 
     def get_cpu_temperature(self):
         cpuInfo = {'temperature': 0, 'color': 'white'}
         try:
-            cpuTemp = float(subprocess.check_output(["vcgencmd measure_temp"], shell=True)
-                .decode('utf8')
-                .strip()
+            cpuTemp = float(subprocess.check_output(["vcgencmd measure_temp"], shell=True)\
+                .decode('utf8')\
+                .strip()\
                 .split('=')[1].split('\'')[0])
             cpuInfo['temperature']=cpuTemp
             if cpuTemp > 40 and cpuTemp < 50:
@@ -136,8 +136,8 @@ class RpiSysInfo():
     def get_disk_usage_list(self):
         items = []
         try:
-            items = [s.split() for s in subprocess.check_output(['df', '-h'], shell=True)
-                .decode('utf8')
+            items = [s.split() for s in subprocess.check_output(['df', '-h'], shell=True)\
+                .decode('utf8')\
                 .splitlines()]
         except Exception as ex:
             print(ex)
