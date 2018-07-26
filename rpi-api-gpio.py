f3bd3c118a8d195d08168abdefeb346455eb0a42
randomsilo <randomsilo@gmail.com>
2018-07-25 21:27:24 -0500

    remove dup keys

--- rpi-api-gpio.py
+++ rpi-api-gpio.py
@@ -32,14 +32,14 @@ def index():
 
     data = {
         "sys_data": rpi_sys_info.sys_data
-        , "os_name": rpi_sys_info.os_name
-        , "cpu_usage_info": rpi_sys_info.cpu_usage_info
-        , "cpu_processor_count": rpi_sys_info.cpu_processor_count
-        , "cpu_core_frequency": rpi_sys_info.cpu_core_frequency
-        , "cpu_core_volt": rpi_sys_info.cpu_core_volt
-        , "cpu_temperature": rpi_sys_info.cpu_temperature
-        , "boot_info": rpi_sys_info.boot_info
-        , "memory_usage_info": rpi_sys_info.memory_usage_info
+        , rpi_sys_info.os_name
+        , rpi_sys_info.cpu_usage_info
+        , rpi_sys_info.cpu_processor_count
+        , rpi_sys_info.cpu_core_frequency
+        , rpi_sys_info.cpu_core_volt
+        , rpi_sys_info.cpu_temperature
+        , rpi_sys_info.boot_info
+        , rpi_sys_info.memory_usage_info
         , "cpu_generic_info": rpi_sys_info.cpu_generic_info
         , "disk_usage_info": rpi_sys_info.disk_usage_info
     }
