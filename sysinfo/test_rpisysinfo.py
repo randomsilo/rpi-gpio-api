ffb24ee93a8eb57b3fc5eb41958868a37d2c5349
randomsilo <randomsilo@gmail.com>
2018-07-25 20:37:24 -0500

    Added all sysinfo methods for display

--- sysinfo/test_rpisysinfo.py
+++ sysinfo/test_rpisysinfo.py
@@ -11,7 +11,18 @@ class RpiSysInfoTestSuite(unittest.TestCase):
     def test_simple(self):
         rpi_sys_info = RpiSysInfo()
         
-        print("=== %s === \n %s" % ("SECTION", rpi_sys_info.get_cpu_generic_details() ))
+        print("\n=== %s === \n%s" % ("os_name", rpi_sys_info.get_os_name() ))
+        print("\n=== %s === \n%s" % ("cpu_usage_info", rpi_sys_info.get_cpu_usage_info() ))
+        print("\n=== %s === \n%s" % ("cpu_processor_count", rpi_sys_info.get_cpu_processor_count() ))
+        print("\n=== %s === \n%s" % ("cpu_core_frequency", rpi_sys_info.get_cpu_core_frequency() ))
+        print("\n=== %s === \n%s" % ("cpu_core_volt", rpi_sys_info.get_cpu_core_volt() ))
+        print("\n=== %s === \n%s" % ("cpu_temperature", rpi_sys_info.get_cpu_temperature() ))
+        print("\n=== %s === \n%s" % ("boot_info", rpi_sys_info.get_boot_info() ))
+        print("\n=== %s === \n%s" % ("memory_usage_info", rpi_sys_info.get_memory_usage_info() ))
+        print("\n=== %s === \n%s" % ("cpu_generic_info", rpi_sys_info.get_cpu_generic_details() ))
+        print("\n=== %s === \n%s" % ("disk_usage_info", rpi_sys_info.get_disk_usage_list() ))
+        print("\n=== %s === \n%s" % ("running_process_info", rpi_sys_info.get_running_process_list() ))
+        print("\n=== %s === \n%s" % ("utility_processor", rpi_sys_info.get_utility_processor() ))
         
         assert True
 
